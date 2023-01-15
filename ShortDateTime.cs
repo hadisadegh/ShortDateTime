d3c216c280ef5b45ceec2afc259347dde5f738ee
hadi <hadisdegh@yahoo.com>
2023-01-15 16:21:18 +0330

    First Commit

---
+++ ShortDateTime.cs
@@ -0,0 +1,24 @@
+﻿namespace Baselayer.Functions
+{
+    public static class ShortDateTime
+    {
+        public static string YearMonthDay(DateTime dateTime)
+        {
+            return dateTime.Year.ToString() + "-" + dateTime.Month.ToString() + "-" + dateTime.Day;
+        }
+
+        public static string YearMonthDayHourseMinute(DateTime dateTime)
+        {
+            return dateTime.Year.ToString() + "-"
+                + dateTime.Month.ToString() + "-"
+                + dateTime.Day + " "
+                + dateTime.Hour + ":"
+                + dateTime.Minute;
+        }
+
+        public static string HourseMinute(DateTime dateTime)
+        {
+            return dateTime.Hour.ToString() +":"+ dateTime.Minute.ToString();
+        }
+    }
+}
